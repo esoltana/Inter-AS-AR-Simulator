6aae8b8003ab4346476e81d9f67814c0265c8463
esoltanaghaei-ubuntu <e.soltanaghaii@gmail.com>
2014-09-10 09:20:38 +0430

    changed function names

--- src/CallGenerator.h
+++ src/CallGenerator.h
@@ -6,27 +6,27 @@ using namespace std;
 class CallGenerator {
 public:
     CallGenerator(int as_num);
-    void readNodeVector(int[] nodeNumArray);
+    void readNodeVector(int nodeNumArray[]);
     void mapNode(int index);
     int mapIndex(int AS_num, int vertex_num);
     void generateCall(double arr, int windowsize, int leadtime,      int slot_length , int flag);
-    void readCommonFile(string dir);
-    void readMatrix(string dir);
+    void readCommonFile(string path);
+    void readprobMatrix(string path);
 
     int source_node, dest_AS, dest_node;
     vector<int> ARvec;
     int Duration;
     double Capacity;
+    
 private:
     int asnum;
     vector<int> nodevec;  // vector of number of nodes in each AS
-
     int total_node;
     double USSTcap, ESTcap;
     int USSTduration, ESTduration_min, ESTduration_max;
     int USSTn, ESTn;
     double zipf_alpha;
-    double theMatrix[75][75];
+    double probMatrix[75][75];
     double USST_EST_prob[2];
     double USST_EST[2];
 
